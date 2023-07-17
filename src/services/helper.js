250c6b8da6fab71d788f19963a12e3f1bf6909b5
kapillohanii <kapillohani023@gmail.com>
2023-07-18 02:55:33 +0530

    sentiment changes

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,3 +1,3 @@
-//export const BASE_URL = "https://space00backend.onrender.com";
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://space00backend.onrender.com";
+//export const BASE_URL = "http://localhost:5000";
 export const API_URL = "http://localhost:4000/sentiment-color";
